81ba04b96cefd109a99c8eecc89a0e0d86e39cbb
Sagar Bera <contact.sagarbera@gmail.com>
2023-01-29 15:42:35 +0530

    added validation schema, middleware

--- src/controllers/coursesController.js
+++ src/controllers/coursesController.js
@@ -1,5 +1,5 @@
-const errorHandler = require("../utils/errorHandler");
-const courseModel = require("../models/courseModel")
+const errorHandler = require("../middleware/errorHandler");
+const courseModel = require("../models/courseModel");
 const getAllCourse = async (req, res) => {
   res.status(200).json({ msg: `All courses` });
 };
@@ -7,10 +7,10 @@ const getSingleCourse = async (req, res) => {
   res.status(200).json({ msg: `Single course` });
 };
 const createCourse = async (req, res) => {
-  let data= req.body
-  let {title,description,videoUrl,topics,duration,category}=data;
-  const caeateCourse= await courseModel.caeate(data)
-  res.status(200).json({ msg: `Create course`,data:caeateCourse });
+  let data = req.body;
+  let { title, description, videoUrl, topics, duration, category } = data;
+  const caeateCourse = await courseModel.create(data);
+  res.status(200).json({ msg: `Create course`, data: caeateCourse });
 };
 const updateCourse = async (req, res) => {
   res.status(200).json({ msg: `Update course` });
